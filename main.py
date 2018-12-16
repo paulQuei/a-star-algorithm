83035d315f53fb3c3ab736e356e8bb80e2dd3c78
paul <paul@Pauls-iMac-Pro.local>
2018-12-16 14:08:50 +0800

    First Update

---
+++ main.py
@@ -0,0 +1,41 @@
+# main.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.patches import Rectangle
+
+import random_map
+import a_star
+
+plt.figure(figsize=(6, 6))
+
+map = random_map.RandomMap()
+
+ax = plt.gca()
+ax.set_xlim([0, map.size])
+ax.set_ylim([0, map.size])
+
+for i in range(map.size):
+    for j in range(map.size):
+        if map.IsObstacle(i,j):
+            rec = Rectangle((i, j), width=1, height=1, color='grey')
+            ax.add_patch(rec)
+        else:
+            rec = Rectangle((i, j), width=1, height=1, edgecolor='grey', facecolor='w')
+            ax.add_patch(rec)
+
+rec = Rectangle((0, 0), width = 1, height = 1, facecolor='b')
+ax.add_patch(rec)
+
+rec = Rectangle((map.size-1, map.size-1), width = 1, height = 1, facecolor='r')
+ax.add_patch(rec)
+
+plt.axis('equal')
+plt.axis('off')
+plt.tight_layout()
+#plt.show()
+
+a_star = a_star.AStar(map)
+a_star.RunAndSaveImage(ax, plt)
+
